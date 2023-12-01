5e8877f8797948873907858548b30d9ee1aa3d87
andlacerda <andlacerdalacerda@gmail.com>
2023-12-01 08:53:52 -0300

    Project Complete

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,20 +1,35 @@
 
-const apiKey = #;
-const apiUrl = #;
+const apiKey = "#" //add your apiKey from Open Weather ; 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const sBox = document.querySelector("#txtCity");
 const sButton = document.querySelector(".btnSearch");
-const element = document.querySelector("#content-align");
-const icon = document.getElementById("element");
+const item = document.querySelector(".content-align");
 
 async function checkWeather(city) {
+
+    try {
     const response = await fetch(apiUrl + `${city}&appid=${apiKey}`);
     var data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " c";
-    element.removeChild(icon);
-
+    console.log(data.weather[0].main);
     
+    if (data.weather[0].main == "Clouds"){
+        document.getElementById("icon").className = "ri-cloud-line weather-icon";
+    }else if (data.weather[0].main == "Clear"){
+        document.getElementById("icon").className = "ri-sun-line weather-icon";
+    }else if (data.weather[0].main == "Rain"){
+        document.getElementById("icon").className = "ri-rainy-line weather-icon";
+    }else if (data.weather[0].main == "Drizzle"){
+        document.getElementById("icon").className = "ri-drizzle-line weather-icon";
+    }else if (data.weather[0].main == "Mist"){
+        document.getElementById("icon").className = "ri-mist-line weather-icon";
+    }
+    }catch(err){
+        document.getElementById("icon").className = "ri-question-mark weather-icon";
+        document.querySelector(".temp").innerHTML = "undefined";
+    }
 }
 
 sButton.addEventListener("click", () =>{
